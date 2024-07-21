1fb18a7bdba650238a15f8adb7e08633dbdb3803
ktcotz <naskret.kamil@gmail.com>
2024-07-21 13:35:18 +0200

    feat: client favicon
    Create favicon with webmanifest.

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,16 +1,3 @@
-import { useEffect } from "react";
-
 export const App = () => {
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch("http://localhost:3000");
-
-      const data = await res.text();
-
-      console.log(data);
-    };
-    fetchData();
-  }, []);
-
   return <h1>Hello Turbo!</h1>;
 };
